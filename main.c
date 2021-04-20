6906e137b98941b0ad0a255e871fe10972a25cad
Matthias Nunes <matthiasnunes@gmail.com>
2021-04-20 19:26:33 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct Chip8
+{
+    uint8_t registers[16];
+    uint8_t memory[4096];
+    uint16_t index;
+    uint16_t pc;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint8_t keypad[16];
+    uint32_t video[64 * 32];
+    uint16_t opcode;
+} Chip8;
+
+int main() {
+    Chip8* a = malloc(sizeof(Chip8));
+    Chip8 a = {.pc = 10};
+    printf("Hello, World!\n");
+    return 0;
+}
