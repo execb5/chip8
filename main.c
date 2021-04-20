5a4a1503d5b670aec571f557a8b317b2a797874b
Matthias Nunes <matthiasnunes@gmail.com>
2021-04-20 20:56:29 -0300

    adds fonts

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Chip8
-{
+#define START_ADDRESS 0x200
+#define END_ADDRESS 0xfff
+#define FONT_SET_START_ADDRESS 0x50
+#define FONT_SET_SIZE 80
+
+uint8_t font_set[FONT_SET_SIZE] =
+        {
+                0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+                0x20, 0x60, 0x20, 0x20, 0x70, // 1
+                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+                0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+
+
+typedef struct Chip8 {
     uint8_t registers[16];
     uint8_t memory[4096];
     uint16_t index;
@@ -18,13 +44,13 @@ typedef struct Chip8
 } Chip8;
 
 int main() {
-    Chip8* a = malloc(sizeof(Chip8));
-    FILE* f = fopen("/Users/mnunes/repos/chip8/pong.ch8", "r");
+    Chip8 *a = malloc(sizeof(Chip8));
+    FILE *f = fopen("/Users/mnunes/repos/chip8/pong.ch8", "r");
     if (!f) {
         return 1;
     }
-    fread(&a->memory[0x200], sizeof(0xfff - 0x200), 1, f);
+    fread(&a->memory[START_ADDRESS], sizeof(END_ADDRESS - START_ADDRESS), 1, f);
     fclose(f);
-    printf("Hello, World!\n");
+    memcpy(&a->memory[FONT_SET_START_ADDRESS], font_set, FONT_SET_SIZE);
     return 0;
 }
