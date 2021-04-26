f0a82cd0cf480b2b433e99abadca5533ce10abdc
Matthias Nunes <matthiasnunes@gmail.com>
2021-04-26 19:58:56 -0300

    places comments to be on the right of the function declaration instead of above

--- include/chip8.h
+++ include/chip8.h
@@ -27,24 +27,15 @@ typedef struct Chip8 {
 
 Chip8* create(char* rom_name);
 void dump_memory_to_file(Chip8* chip, char* memory_file_name);
-// CLS
-void op_00e0(Chip8* chip);
-// RET
-void op_00ee(Chip8* chip);
-// JP addr
-void op_1nnn(Chip8* chip);
-// CALL addr
-void op_2nnn(Chip8* chip);
-// SE Vx, byte
-void op_3xkk(Chip8* chip);
-// SNE Vx, byte
-void op_4xkk(Chip8* chip);
-// SE Vx, Vy
-void op_5xy0(Chip8* chip);
-// LD Vx, byte
-void op_6xkk(Chip8* chip);
-// ADD Vx, byte
-void op_7xkk(Chip8* chip);
+void op_00e0(Chip8* chip); // CLS
+void op_00ee(Chip8* chip); // RET
+void op_1nnn(Chip8* chip); // JP addr
+void op_2nnn(Chip8* chip); // CALL addr
+void op_3xkk(Chip8* chip); // SE Vx, byte
+void op_4xkk(Chip8* chip); // SNE Vx, byte
+void op_5xy0(Chip8* chip); // SE Vx, Vy
+void op_6xkk(Chip8* chip); // LD Vx, byte
+void op_7xkk(Chip8* chip); // ADD Vx, byte
 void destroy(Chip8* chip);
 
 #endif /* CHIP8_H */
